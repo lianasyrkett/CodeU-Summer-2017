f46b94fb7ef063fe05aa823de098148686b4e388
seibelsabrina <seibelsabrina@gmail.com>
2017-08-01 09:39:56 -0400

    Updated Snapshotter.java

--- src/codeu/chat/server/Snapshotter.java
+++ src/codeu/chat/server/Snapshotter.java
@@ -1,9 +1,8 @@
 package codeu.chat.server;
-// Where should this file and StoredData be saved?
-//  In the same folder as Model.java?
 
 import codeu.chat.server.Model;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
@@ -11,31 +10,27 @@ import java.io.ObjectOutputStream;
 public class Snapshotter implements Runnable {
 
     private Model model;
+    private File fp;
 
-    public Snapshotter(Model model) {
+    public Snapshotter(Model model, File fp) {
         this.model = model;
+        this.fp = fp;
     }
 
     public void run() {
         boolean infinite = true; 
-        while (infinite = true) { // I feel like this isnt a good way to make an infinite while loop but
+        while (infinite = true) {
             sleep();
-            serialize();
+            serialize(fp);
         }
         
     }
 
-    public void serialize() {
+    public void serialize(File fp) {
         try {
-        // StoredData.txt is a file to save the serialized data to
-        FileOutputStream fs = new FileOutputStream("StoredData.txt");
-
+        FileOutputStream fs = new FileOutputStream(fp);
         ObjectOutputStream os = new ObjectOutputStream(fs);
-
-        // should I be serializing a Model instance I create?
         os.writeObject(model); 
-        
-        // close the streams
         os.close();
         fs.close();
 
@@ -46,12 +41,9 @@ public class Snapshotter implements Runnable {
 
     public void sleep() {
         try {
-            // makes thread sleep for 30 seconds - therefore it updates the data every 30 seconds
             Thread.sleep(30000);
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
     }
-    // where should the deserialization occur? 
-    // when it checks for the optional flag in ServerMain?
 }
